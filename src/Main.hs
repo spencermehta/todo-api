3da94360f3f922c299713ffbfdd73e566f7ea696
Spencer <spencermehta@gmail.com>
2020-04-07 23:49:49 +0100

    semi working add

--- src/Main.hs
+++ src/Main.hs
@@ -1,46 +1,72 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE DeriveGeneric #-}
+
 module Main where
 
 import Web.Scotty
 import Data.Monoid ((<>))
 import Data.Aeson (FromJSON, ToJSON)
 import GHC.Generics
+import Control.Applicative
+import Control.Monad.IO.Class
+import Database.SQLite.Simple
+import Database.SQLite.Simple.FromRow
 
-data Item = Item { itemkId :: Int, itemDesc :: String }
+data Item = Item { itemDesc :: String }
   deriving (Show, Generic)
 
 type ItemList = [Item]
 
-instance ToJSON Task
-instance FromJSON Task
+instance FromRow Item where
+    fromRow = Item <$> field
 
-item1 :: Item
-item1 = Item {itemId = 1, itemDesc = "Complete JSON API"}
+instance ToRow Item where
+    toRow i = [toField $ itemText i, toField $ finished i, toField $ checklist i]
 
-item2 :: Item
-item2 = Item {itemId = 2, itemDesc = "Implement POST requests"}
+instance ToJSON Item
+instance FromJSON Item
 
-allItems :: ItemList
-allItems = [item1, item2]
+withConn :: String -> (Connection -> IO ()) -> IO ()
+withConn dbName action = do
+   conn <- open dbName
+   action conn
+   close conn
 
-addItem :: ItemList -> Item -> ItemList
-addItem = flip (:)
+addItem :: String -> IO ()
+addItem itemDesc = withConn "todo.db" $
+          \conn -> do
+            execute conn "INSERT INTO todo (itemDesc) VALUES (?)"
+              (Only (itemDesc :: String))
+            r <- query_ conn "SELECT * FROM todo" :: IO [(Int, String)]
+            mapM_ print r
+-- addItem :: Item -> IO ()
+-- addItem (Item item) = withConn "todo.db" $
+--           \conn -> do
+--             execute conn "INSERT INTO todo (itemDesc) VALUES (?)"
+--               (Only item)
+--             r <- query_ conn "SELECT * FROM todo WHERE itemId = 10" :: IO [(Int, String)]
+--             mapM_ print r
 
-matchesId :: Int -> User -> Bool
-matchesId id user = userId user == id
+-- matchesId :: Int -> Item -> Bool
+-- matchesId id item = itemId item == id
 
 routes :: ScottyM ()
-routes = do
+routes conn = do
     
   get "/items" $ do
-    json allItems
+    items <- liftIO (query_ conn "select * from todo" :: IO [Item])
 
-  post "/items" $ do
-    item <- jsonData :: ActionM Item
-    addItem item
+  post "/items:item" $ do
+    item <- param "item"
+    liftIO (addItem item)
+    text ("hello !")
+
+  -- post "/items" $ do
+  --   item <- jsonData :: ActionM Item
+  --   addItem item
 
 main :: IO ()
 main = do
   putStrLn "Starting Server..."
-  scotty 3000 routes
+  conn <- open "todo.db"
+  scotty 3000 $ routes conn
